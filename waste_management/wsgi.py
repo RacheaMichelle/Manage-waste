500883e6cf9cafcae3c928f54b89664f545265d1
RacheaMichelle <rachealnannozi77@gmail.com>
2025-10-20 13:44:57 +0300

    fixed deployment

--- waste_management/wsgi.py
+++ waste_management/wsgi.py
@@ -1,16 +1,6 @@
 import os
-import sys
 from django.core.wsgi import get_wsgi_application
-
-# Add the project directory to the Python path
-project_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.insert(0, project_dir)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'waste_management.settings')
 
-try:
-    application = get_wsgi_application()
-except Exception as e:
-    # If there's an error, print it but don't crash
-    print(f"Error initializing Django: {e}")
-    raise
+application = get_wsgi_application()
